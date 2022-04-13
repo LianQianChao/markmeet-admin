9e4d58e0010013bf1a4a27023ebe6c2ec51bf0eb
LianQianChao <i@lchao.cc>
2022-04-13 21:25:49 +0800

    feat:init

---
+++ src/main/java/com/meet/markmeetadmin/MarkmeetAdminApplication.java
@@ -0,0 +1,13 @@
+package com.meet.markmeetadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkmeetAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkmeetAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meet/markmeetadmin/MarkmeetAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meet.markmeetadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkmeetAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
